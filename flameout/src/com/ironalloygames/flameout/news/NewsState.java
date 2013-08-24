42ad5030741659ea7bd912a5868f2a4b5a4dc1a9
Iron Alloy Games <admin@ironalloygames.com>
2013-08-24 16:17:21 -0700

    next

--- flameout/src/com/ironalloygames/flameout/news/NewsState.java
+++ flameout/src/com/ironalloygames/flameout/news/NewsState.java
@@ -2,11 +2,28 @@ package com.ironalloygames.flameout.news;
 
 import java.util.Random;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.ironalloygames.flameout.GameState;
 import com.ironalloygames.flameout.news.NewsStory.Tag;
 
 public class NewsState extends GameState {
 	Random rand = new Random();
+
+	OrthographicCamera camera = new OrthographicCamera(800,600);
+
+	static BitmapFont headline, subheadline, regular;
+
+	static Texture background;
+
+	String newspaperName = "";
+
+	protected String rStr(String[] options){
+		return options[rand.nextInt(options.length)];
+	}
 
 	public NewsState(Tag leadingStoryTag){
 		NewsStory[] stories = {
@@ -30,6 +47,16 @@ public class NewsState extends GameState {
 			}
 		}
 
+		if (headline == null){
+			headline = new BitmapFont(Gdx.files.internal("ns-headline.fnt"), Gdx.files.internal("ns-headline_00.png"), false);
+			subheadline = new BitmapFont(Gdx.files.internal("ns-subheadline.fnt"), Gdx.files.internal("ns-subheadline_00.png"), false);
+			regular = new BitmapFont(Gdx.files.internal("ns-regular.fnt"), Gdx.files.internal("ns-regular_00.png"), false);
+
+			background = new Texture(Gdx.files.internal("newspaper-background.png"));
+		}
+
+		newspaperName = "The " + rStr(new String[]{"Daily", "Weekly", "Valiant", "Worldwide"}) + " " + rStr(new String[]{"Herald", "Devastator", "Truthifier", "Chronicle"});
+
 		System.out.println("Newspaper: " + leadingStory + " " + secondStory);
 	}
 
@@ -38,8 +65,25 @@ public class NewsState extends GameState {
 
 	@Override
 	public void update() {
+		batch.setProjectionMatrix(camera.combined);
+		batch.begin();
 
+		batch.draw(background, -background.getWidth() / 2, -background.getHeight() / 2);
+
+
+		drawTextCentered(subheadline, newspaperName, 0, 263);
+
+		drawTextCentered(headline, leadingStory.getHeadline(), 0, 230);
+
+		batch.end();
 	}
+
+	void drawTextCentered(BitmapFont font, String s, int x, int y){
+		font.setColor(Color.BLACK);
+		font.draw(batch, s, x - font.getBounds(s).width / 2, y);
+	}
+
+
 	@Override
 	public void render() {
 
